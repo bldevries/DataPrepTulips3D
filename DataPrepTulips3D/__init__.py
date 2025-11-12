6e253f87921710a61d687ad913d2aea18a8a7e05
Ben de Vries <bldevries@gmail.com>
2025-11-12 16:08:28 +0100

    Added notebook and some fixes

--- DataPrepTulips3D/__init__.py
+++ DataPrepTulips3D/__init__.py
@@ -3,5 +3,5 @@ import importlib.metadata
 __version__ = importlib.metadata.version('DataPrepTulips3D')
 
 from DataPrepTulips3D.mesa_data import loadMesaData
-from DataPrepTulips3D.Data1D import Data1D
+from DataPrepTulips3D.Data1D import Data1D, save_Data1D_to_pickle, load_Data1D_from_pickle
 
