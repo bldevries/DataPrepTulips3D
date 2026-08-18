ffadd017aa5669eb83e2daa3db7cba5f633cea40
Ben de Vries <bldevries@gmail.com>
2026-08-18 10:08:21 +0200

    Little test

--- DataPrepTulips3D/__init__.py
+++ DataPrepTulips3D/__init__.py
@@ -2,6 +2,6 @@
 import importlib.metadata
 __version__ = importlib.metadata.version('DataPrepTulips3D')
 
-from DataPrepTulips3D.mesa_data_interp import loadMesaData, save_to_pickle, load_from_pickle
+from DataPrepTulips3D.mesa_data_interp import *
 
 
